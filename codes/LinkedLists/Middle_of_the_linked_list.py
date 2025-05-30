90523b4a01cc18e7236ae6eab4f41e828572fde3
AlvaroDutra <dutalvaro@gmail.com>
2025-05-30 09:44:13 -0400

    add solution Middle of the linked list

--- codes/LinkedLists/Middle_of_the_linked_list.py
+++ codes/LinkedLists/Middle_of_the_linked_list.py
@@ -1,1 +1,15 @@
 # LeetCode 876. Middle of the linked list
+
+class Solution:
+    def middleNode(self, head):
+        ahead = head
+
+        while ahead and ahead.next:
+            ahead = ahead.next.next
+            head = head.next
+        return head
+
+# this problem can be solved using two pointers technique
+# one pointer moves two steps at a time and the other moves one step at a time
+# when the first pointer reaches the end of the list, the second pointer will be at the middle
+# its like a table game where one player moves two steps and the other moves one step        
