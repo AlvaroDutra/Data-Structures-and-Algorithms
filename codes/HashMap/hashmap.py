e68174ac122c15a77d7429774f969a22495ff1a0
AlvaroDutra <dutalvaro@gmail.com>
2025-07-21 11:24:02 -0400

    add bonus content

--- codes/HashMap/hashmap.py
+++ codes/HashMap/hashmap.py
@@ -1,7 +1,7 @@
 class HashMap:
     def __init__(self, size = 100) -> None:
         self.size = size
-        self.buckets = [[] for l in range(size)]
+        self.buckets = [[] for _ in range(size)]
 
     def _hash(self, key):
         return hash(key) % self.size
@@ -16,3 +16,22 @@ class HashMap:
                 return
 
         bucket.append([key, value])   
+
+    def get(self, key):
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        for k, v in bucket:
+            if k == key:
+                return v
+        
+        return None
+
+    def delete(self, key):
+        index = self._hash(key)
+        bucket = self.buckets[index]
+
+        for i, (k,v) in enumerate(bucket):
+            if k == key:
+                del bucket[i]
+                return
